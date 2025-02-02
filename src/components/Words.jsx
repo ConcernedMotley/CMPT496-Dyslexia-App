1b3ff97e50629feb9178e32731984a8d4bd6f6e0
Sankalp Shrivastav <123102312+ConcernedMotley@users.noreply.github.com>
2025-02-02 01:14:24 -0700

    Added link

--- src/components/Words.jsx
+++ src/components/Words.jsx
@@ -3,6 +3,9 @@ import '../styles/words.css'
 import '../styles/colors.css'
 import Blon from '../audio/Blon.wav'
 
+// Speech To Text
+// https://console.cloud.google.com/vertex-ai/studio/speech
+
 export function Word ({wordText}) {
 
     const playSound = () => {
