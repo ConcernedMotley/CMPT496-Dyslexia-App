dea7713aec5205d85fc3217d9f062194b3fbcc47
Sankalp Shrivastav <123102312+ConcernedMotley@users.noreply.github.com>
2025-02-01 23:06:15 -0700

    testing button components with values

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import './App.css'
+import './styles/App.css'
+import { Button01, Button02 } from './components/Buttons'
 
 function App() {
 
   return (
     <>
     <h1>Testing</h1>
+    <Button01 buttonText="Hello World!"/>
+    <Button02 buttonText="Hello World Again!" />
     </>
   )
 }
---
+++ src/components/Buttons.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+
+export function Button01 ({buttonText}) {
+
+    return (
+        <>
+        <h1>{buttonText} I am button 01!</h1>
+        </>
+    )
+}
+
+export function Button02 ({buttonText}) {
+    return (
+        <>
+        <h1>{buttonText} I am button 02!</h1>
+        </>
+    )
+}
+
+Button01.propTypes = {
+    buttonText: PropTypes.string.isRequired,
+}
+
+Button02.propTypes = {
+    buttonText: PropTypes.string.isRequired,
+}
