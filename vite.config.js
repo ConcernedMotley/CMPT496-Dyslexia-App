c7487d27e8f2fa84af2dd25422d7e715068edbbc
Sankalp Shrivastav <123102312+ConcernedMotley@users.noreply.github.com>
2025-02-01 23:23:11 -0700

    corrected base name in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ConcernedMotley/",
+  base: "/CMPT496-Dyslexia-App/",
 });
