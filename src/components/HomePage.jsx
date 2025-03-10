45c248eb976be9141704fe4b5a72e52e14683a7d
TayCarr <117793653+TayCarr@users.noreply.github.com>
2025-03-10 13:11:08 -0600

    starting UX

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,19 +1,25 @@
 // src/components/HomePage.jsx
 import { Link } from 'react-router-dom';
-import '../styles/LandingPageStyle.css';
+import '../styles/general_style.css';
+import '../styles/home_style.css';
 
 function HomePage() {
   return (
+    
     <div className="home-container">
-      <h1>Dyslexia App</h1>
-      <h1>Welcome to the Home Page</h1>
-      <Link to="/RegisterPage">
-        <button className="home-button">Register an Account</button>
-      </Link>
-      <Link to="/LoginPage">
-        <button className="home-button">Login if you have an account</button>
-      </Link>
-    </div>
+    <h1 className='title text-style-lexend-bold'>
+      <span className="purple-text">Lumo</span>
+      <span className="turquoise-text">Learn</span>
+    </h1>
+    <Link to="/LoginPage">
+      <button className="login-button purple-button">Login</button>
+    </Link>
+    <Link to="/RegisterPage">
+      <button className="register-button turquise-button ">New User</button>
+    </Link>
+  </div>
+
+        
   );
 }
 
--- src/components/RegisterPage.jsx
+++ src/components/RegisterPage.jsx
@@ -1,10 +1,12 @@
 // src/components/RegisterPage.jsx
 import { Link } from 'react-router-dom';
 import { useState } from "react";
-import '../styles/LandingPageStyle.css';
+import '../styles/general_style.css';
+import '../styles/register_style.css';
 
 const RegisterPage = () => {
-    const [formData, setFormData] = useState({ username: "", password: "" });
+    const [formData, setFormData] = useState({ 
+      username: "", password: "", repeatPassword: "" });
 
 const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -12,42 +14,81 @@ const handleChange = (e) => {
 
 const handleSubmit = (e) => {
     e.preventDefault();
+    //TODO logic for form submission
     console.log("Submitted Data:", formData);
   };
 
 
   return (
-    <div className="register-container">
-        <h1>Fill details to register</h1>
-         <form onSubmit={handleSubmit} className="register-form">
-            <label>Username:</label>
+    <><div className='greeting'>
+      <h1 className="purple-text">Lets create a new account</h1>
+      <p className='reg-desc'>Create an account by filling in the boxes below</p>
+    </div><div className='registration-container'>
+        <form onSubmit={handleSubmit} className="register-form">
+          <div className='field'>
+            <div className="input-background"></div>
+            <label htmlFor="username" className="input-label">Email or Username</label>
             <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-            required
-            />
+              type="text"
+              id="username"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              placeholder="Email or Username"
+              required />
+          </div>
 
-            <label>Password:</label>
+          <div className="field">
+            <div className="input-background"></div>
+            <label htmlFor="password" className="input-label">Password</label>
             <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-            />
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder="Password"
+              required />
+          </div>
 
-            <Link to="/HomePage">
-                <button className="register-button">Go Back!</button>
-            </Link>
+          <div className="field">
+            <div className="input-background"></div>
+            <label htmlFor="repeatPassword" className="input-label">Repeat Password</label>
+            <input
+              type="password"
+              id="repeatPassword"
+              name="repeatPassword"
+              value={formData.repeatPassword}
+              onChange={handleChange}
+              placeholder="Repeat Password"
+              required />
+          </div>
+          {/*<Link to="/LoginPage">
+          <button className='register turquise-button' type="submit">Create Account</button>
+    </Link>*/}
+          <button className="signup-button" type="submit">Create Account</button>
+        </form>
 
-             <Link to="/LoginPage">
-                <button type="submit">Register!</button>
-            </Link>
-      </form>
 
-    </div>
+        <div >
+          <p className='other-reg-desc'>Or sign up with</p>
+          <div className="form-buttons">
+          <button type="button" className="apple-button">Apple</button>
+          <button type="button" className="google-button">Google</button>
+          </div>
+        </div>
+        <div className="terms-link">
+          <Link to="/terms-and-conditions">Our Policy and Terms & Conditions</Link>
+        </div>
+
+
+        {/*<Link to="/HomePage">
+        <button className="register-button">Go Back!</button>
+</Link>*/}
+
+
+
+      </div></>
   );
 }
 
