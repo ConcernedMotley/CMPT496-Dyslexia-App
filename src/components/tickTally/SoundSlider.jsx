d59443e5021051d808dd1e253d18a2c798c9806e
Sankalp Shrivastav <123102312+ConcernedMotley@users.noreply.github.com>
2025-03-19 13:48:20 -0600

    Slider added
    Added slider component and new package.

--- src/components/tickTally/SoundSlider.jsx
+++ src/components/tickTally/SoundSlider.jsx
@@ -1,7 +1,19 @@
+import ReactSlider from "react-slider";
+
 export default function SoundSlider() {
     return (
         <div className="horizontal-flex sound-slider">
+            <ReactSlider
+            
+                className="tick-tally-slider"
+                trackClassName="tick-tally-slider-track"
+                thumbClassName="tick-tally-slider-thumb"
+                markClassName="tick-tally-slider-mark"
+                marks={1}
+                min={0}
+                max={7}
 
+            />
         </div>
     );
 }
